2017b1ef45a6bf10e0fdeb8812c7b21359e00e3b
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-11 23:45:24 +0200

    navbar updated

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavbarStyles.css";
-const Navbar = () => {
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <nav className='navbar'>
-      <ul className='navbar-link-container'>
-        <li>ChatFlow</li>
-        <li>Home</li>
-        <li>Pricing</li>
-        <li>Support</li>
-        <li>Contact</li>
-      </ul>
-
-      <article className='navbar-buttons-container'>
-        <button>Login</button>
-        <button>Register</button>
-      </article>
+      <div className='navbar-container'>
+        <a href='/' className='navbar-logo'>
+          Chatflow
+        </a>
+        <div
+          className={isOpen ? "menu-icon open" : "menu-icon"}
+          onClick={toggle}
+        >
+          <div className='menu-icon__line' />
+          <div className='menu-icon__line' />
+          <div className='menu-icon__line' />
+        </div>
+        <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
+          <li className='nav-item'>
+            <a href='/' className='nav-link'>
+              Home
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/pricing' className='nav-link'>
+              Pricing
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/support' className='nav-link'>
+              Support
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/contact' className='nav-link'>
+              Contact
+            </a>
+          </li>
+        </ul>
+        <div className='navbar-buttons'>
+          <button>login</button>
+          <button>question</button>
+        </div>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
